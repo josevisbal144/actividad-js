1fd7bc808c4bdf8dc48d22fae8ec69d28bce0d3f
jose visbal <josevisbal144@gmail.com>
2026-05-27 15:51:37 -0500

    primer commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "./styles/globals.css";
+
+const botonAzul = document.getElementById("boton-azul");
+botonAzul.addEventListener("click", function () {
+    alert("¡Hola! Has hecho clic en el botón azul.");
+}
+)
+
+
+    
